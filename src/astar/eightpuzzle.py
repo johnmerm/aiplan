4299d6ce4951dc1e3c519f0c2a05b3add785721b
ioannis.mermigkis <ioannis.mermigkis@siemens-enterprise.com>
2014-02-03 16:42:53 +0200

    Done

--- src/astar/eightpuzzle.py
+++ src/astar/eightpuzzle.py
@@ -69,12 +69,14 @@ def reconstruct_path(came_from,node,start):
             return list(reversed(p))
 
 def validatePath(path,start,end):
+    visited = {start}
     valid = (path[0] == start and path[-1]==end)
-    if valid: 
-        for i in range(1,len(path)):
-            n = path[i]
-            p = path[i-1]
-            valid = valid and (n in neighbors(p))
+    for i in range(1,len(path)):
+        if not valid : return False 
+        n = path[i]
+        p = path[i-1]
+        valid = valid and (not n in visited) and (n in neighbors(p))
+        visited.add(n)
     return valid
 
 def astar(start,end,heuristic=lambda s:manhattan(s),dist=lambda c,n:1):
@@ -87,61 +89,73 @@ def astar(start,end,heuristic=lambda s:manhattan(s),dist=lambda c,n:1):
     f_score = {start:heuristic(start)}
     
     while len(openset) > 0:
-        current = min(openset,key = lambda o: f_score[o])
-        if current == end:
-            return came_from,depth
         
-        openset.remove(current)
-        closedset.add(current)
-        for n in neighbors(current):
-            if n in closedset:
-                continue
-            tentative_g = g_score[current]+dist(current,n)
-            if (not n in openset or tentative_g<g_score[n]):
-                came_from[n] = current
-                g_score[n] = tentative_g
-                f_score[n] = g_score[n] +heuristic(n)
-                if not n in openset:
-                    openset.add(n)
-                    depth[n] = depth[current]+1
+        current = min(openset,key = lambda o: f_score[o])
+        #currents = [c for c in openset if f_score[c] == f_score[current]]
+        currents=[current]
+        for current in currents:
+            if current == end:
+                return came_from,depth
+            
+            openset.remove(current)
+            closedset.add(current)
+            for n in neighbors(current):
+                if n in closedset:
+                    continue
+                tentative_g = g_score[current]+dist(current,n)
+                if (not n in openset or tentative_g<g_score[n]):
+                    came_from[n] = current
+                    g_score[n] = tentative_g
+                    f_score[n] = g_score[n] +heuristic(n)
+                    if not n in openset:
+                        openset.add(n)
+                        depth[n] = depth[current]+1
     return None
 
 
 
-def breadth_first(start,d):
-    depth = {start:0}
+def breadth_first(start,d,candidates=None):
+    depth = 0;
+    visited=set()
     openset = {start}
-    sd = set()
-    while len(openset)>0 :
-        for s in list(openset):
-            openset.remove(s)
-            v = depth[s]
-            if v == d:
-                sd.add(s)
-                continue
-            ns = neighbors(s)
-            for n in ns:
-                if not n in depth:
-                    openset.add(n)
-                    depth[n] = v+1
-    return sd,depth
+    while depth<=d:
+        expand = {n for p in openset for n in neighbors(p)  if not n in visited}
+        visited.update(openset)
+        openset = expand
+        
+        found=set()
+        if candidates!=None:
+            found = openset.intersection(candidates)
+            
+        depth += 1
+        print(depth,len(openset)," ".join(found))
+    return openset
 
 def test(start):
     end = "012345678"
     came_from,depth = astar(start,end)
     
     path = reconstruct_path(came_from, end, start)
-    print(depth[end],len(path),validatePath(path,start,end))
-
-def testDepth(start):
-    end = "012345678"
-    sd,depth = breadth_first(start, end)
+    print(len(path))
+    print(path)
     
-    print(len(sd))
+    
+def testManhattan():
+    seq22 = ['164870325', '164807325', '164827305', '164827350', '164820357', '164802357', '104862357', '140862357', '142860357', '142867350', '142867305', '142807365', '142087365', '142387065', '142387605', '142307685', '142370685', '142375680', '142375608', '142305678', '102345678', '012345678']
+    seq24 = ['164870325', '164875320', '164875302', '164805372', '164085372', '164385072', '164385702', '164305782', '104365782', '014365782', '314065782', '314605782', '314650782', '314652780', '314652708', '314652078', '314052678', '014352678', '104352678', '140352678', '142350678', '142305678', '102345678', '012345678']
+    m22 = [manhattan(s) for s in seq22]
+    m24 = [manhattan(s) for s in seq24]
+    
+    print (m22)
+    print (m24)
     
 test("164870325")
-test("817456203")
-#testDepth("012345678",27)
+#test("817456203")
+#sd = breadth_first("012345678",27,{"164870325","817456203"})
+#print(len(sd))
+
+
+#testManhattan()
 
 
         
